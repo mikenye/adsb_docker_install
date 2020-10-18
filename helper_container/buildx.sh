c5847c37c38eefa801e9ae1c944aea873ba45f04
Mike Nye <mike.nye@gmail.com>
2020-10-18 22:20:20 +0800

    Remove armv6

--- helper_container/buildx.sh
+++ helper_container/buildx.sh
@@ -5,7 +5,7 @@ pushd ./helper_container || exit 1
 
 REPO=mikenye
 IMAGE=adsb_docker_install_helper
-PLATFORMS="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+PLATFORMS="linux/386,linux/amd64,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
